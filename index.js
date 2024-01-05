769f51c1499c3eeb612a7cac7c6c7a33d4491299
iKataria <17ktaria@gmail.com>
2024-01-05 09:17:42 -0500

    Repeated code refactored

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = new express();
+const port = 4000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render("index");
+})
+
+app.get('/about', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render("about");
+})
+
+app.get('/contact', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render("contact");
+})
+
+app.get('/post', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render("post");
+})
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
